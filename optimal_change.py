c6c4b667470ee1d94d580343aa00bf03859d00b1
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-04 13:29:32 -0500

    Shawn's assessment solution v1

--- optimal_change.py
+++ optimal_change.py
@@ -34,7 +34,8 @@ def determine_change(amount_owed, amount_paid):
     # these amounts are all *100 from monetary amounts and represent
     # the correspoding currency amounts in order
     denomination_list = [10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1]
-    change = (amount_paid - amount_owed) * 100
+    change = int(amount_paid*100) - int(amount_owed*100)
+
     #this store the change amount broken down by denomination
     change_list = []
     # case were no change is owed
@@ -48,7 +49,7 @@ def determine_change(amount_owed, amount_paid):
     # case where change is owed, check the amount of each
     # of each denomination owed and add it to change_list
     for index, amount in enumerate(denomination_list):
-        number_of_denom = change // amount
+        number_of_denom = int(change // amount)
         change_list.append(number_of_denom)
         change -= number_of_denom * amount
 
@@ -64,8 +65,10 @@ def determine_change(amount_owed, amount_paid):
 # to the amount owed from CHANGE_LIST for that denomination
 
 def print_change(change_list, amount_owed, amount_paid):
-
-
+    # format amount_owed and amount_paid so they always show 2 decimal places
+    amount_owed = "{:.2f}".format(amount_owed)
+    amount_paid = "{:.2f}".format(amount_paid)
+    
     # case where no change due
     if change_list == []:
         return "We're even steven."
@@ -74,9 +77,26 @@ def print_change(change_list, amount_owed, amount_paid):
     if change_list == None:
         return "You owe more money."
 
-    
-    
-    return ""
+    # record the total number of denominations owed
+    total_num_denoms_owed = check_non_zeros(change_list)
+
+    output_change_str = f"The optimal change for an item that costs ${amount_owed} with an amount paid of ${amount_paid} is "
+
+    for index, amount in enumerate(change_list):
+        if amount > 0:
+            # multiple change amounts remain to append
+            if check_non_zeros(change_list) > 1:
+                output_change_str += change_string(index, amount) + ", "
+            # indicate no more change at the current denomination is owed
+                change_list[index] = 0
+            # case where 1 change denomination remains
+            else:
+                output_change_str += f"{'and ' if total_num_denoms_owed > 1 else ''}" + change_string(index, amount) + "."
+                break
+
+    return output_change_str
+
+
 # change string takes in two arguments - the DENOMINATION_INDEX and AMOUNT
 # denomination indices map to their index in DENOM_STR_LIST
 # CHANGE_STRING returns a string of the format f"{amount} {denom_str}?"s"}
@@ -90,6 +110,16 @@ def change_string(denomination_index, amount):
     if type_change_owed_str == "penn":
         return f"{amount} {type_change_owed_str}{'ies' if amount > 1 else 'y'}"
 
+    # return
     return f"{amount} {type_change_owed_str}{'s' if amount > 1 else ''}"
 
 
+# CHECK_NON_ZEROS takes a number INPUT list and returns the number of non-zero elements
+def check_non_zeros(input):
+
+    non_zeros = 0
+    for elem in input:
+        if elem != 0:
+            non_zeros += 1
+    
+    return non_zeros
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -3,8 +3,11 @@ from optimal_change import optimal_change
 from optimal_change import determine_change
 from optimal_change import print_change
 from optimal_change import change_string
+from optimal_change import check_non_zeros
 
 class OptimalChangeTests(unittest.TestCase):
+
+    maxDiff = None
 
     def test_case_negative_number_input(self):
         output = optimal_change(-1, 1)
@@ -30,6 +33,10 @@ class OptimalChangeTests(unittest.TestCase):
     def test_determine_change(self):
         output = determine_change(100.00, 112.12)
         self.assertEqual(output, [0, 0, 0, 1, 0, 2, 0, 1, 0, 2])
+    
+    def test_determine_change2(self):
+        output = determine_change(62.13, 63)
+        self.assertEqual(output, [0, 0, 0, 0, 0, 0, 3, 1, 0, 2])
 
     def test_change_str(self):
         output = change_string(9, 2)
@@ -46,6 +53,39 @@ class OptimalChangeTests(unittest.TestCase):
     def test_change_str4(self):
         output = change_string(2, 1)
         self.assertEqual(output, "1 $20 bill")
+
+    def test_check_non_zero1(self):
+        output = check_non_zeros([0, 1, 2, 3, 0])
+        self.assertEqual(output, 3)
+    
+    def test_check_non_zero2(self):
+        output = check_non_zeros([0, 0, 0])
+        self.assertEqual(output, 0)
+
+    def test_output_change_string(self):
+        output = print_change([0, 0, 0, 1, 0, 2, 0, 1, 0, 2], 100.00, 112.12)
+        self.assertEqual(output, "The optimal change for an item that costs $100.00 with an amount paid of $112.12 is 1 $10 bill, 2 $1 bills, 1 dime, and 2 pennies.")
+
+    def test_out_change_string2(self):
+        output = print_change([0, 0, 0, 1, 0, 0, 0, 0, 0, 0], 10, 20)
+        self.assertEqual(output, "The optimal change for an item that costs $10.00 with an amount paid of $20.00 is 1 $10 bill.")
+
+    def test_optimal_change(self):
+        output = optimal_change(10, 20)
+        self.assertEqual(output, "The optimal change for an item that costs $10.00 with an amount paid of $20.00 is 1 $10 bill.")
+
+    def test_optimal_change2(self):
+        output = optimal_change(100, 112.12)
+        self.assertEqual(output, "The optimal change for an item that costs $100.00 with an amount paid of $112.12 is 1 $10 bill, 2 $1 bills, 1 dime, and 2 pennies.")
+    
+    def test_optimal_change3(self):
+        output = optimal_change(31.51, 50)
+        self.assertEqual(output, "The optimal change for an item that costs $31.51 with an amount paid of $50.00 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+    def test_optimal_change4(self):
+        output = optimal_change(62.13, 100)
+        self.assertEqual(output, "The optimal change for an item that costs $62.13 with an amount paid of $100.00 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
 
 if __name__ == '__main__':
     unittest.main()
